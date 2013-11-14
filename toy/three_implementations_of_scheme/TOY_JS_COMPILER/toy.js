129ba711e60f87f791d3c832970ef0991b397876
Yiyi Wang <shd101wyy@sina.com>
2013-11-13 22:38:19 -0600

    I give up compiling to javascript cuz idk how to solve closure problem

--- toy/three_implementations_of_scheme/TOY_JS_COMPILER/toy.js
+++ toy/three_implementations_of_scheme/TOY_JS_COMPILER/toy.js
@@ -247,6 +247,9 @@ var parser = function(l)
         	*/
         if(l[0]==":")
        		return cons("keyword", cons(l.slice(1), build_nil()))
+        /*if(l == "+" || l == "-" || l =="*" || l =="/" || l =="%"
+            || l == "<" || l =="eq?")
+            return op_table[l];
         if(l[0]=='"') return l;
        	if(isNumber(l) || isRatio(l)) return l;
        	if(check_invalid_name(l) === false && (l!=="+" && l!=="-" && l!=="*" && l!=="/" && l!=="%" && 
@@ -261,7 +264,7 @@ var parser = function(l)
        			INVALID_NAME_TABLE_LENGTH++; // increase length
        			return var_name;
        		}
-       	}
+       	}*/
         return l;
     }
     // done
@@ -725,8 +728,8 @@ var compiler = function(exp)
         {
         	return compile_dictionary(cdr(exp));
         }
-        else if (tag === "+" || tag === "-" || tag === "*" || tag === "/" || tag === "%"
-                || tag === ">" || tag === "<" || tag ==="<=" || tag ===">=" || tag ==="==" || tag === "!="
+        else if (/*tag === "+" || tag === "-" || tag === "*" || tag === "/" || tag === "%"
+                ||*/ tag === ">" || tag === "<" || tag ==="<=" || tag ===">=" || tag ==="==" || tag === "!="
                 || tag === "===" || tag === "!==")
         {
             if(tag === "==") tag = "==="
@@ -734,6 +737,8 @@ var compiler = function(exp)
         	// return compile_ar(cadr(exp), caddr(exp), tag)
         	return compiler(cadr(exp))+tag+compiler(caddr(exp))
         }
+        else if (tag === "typeof")
+            return "typeof("+compiler(cadr(exp))+")"
         else if (tag === "new")
         {
             return /*" new " +*/ compiler(cadr(exp));
@@ -769,42 +774,284 @@ var compiler = function(exp)
         }
 	}
 }
-
+var op_table  = {
+    "+":"toy_add$", "-":"toy_sub$", "*":"toy_mul$", "/":"toy_div$", "%":"toy_rem",
+    "<":"toy_lt$", "eq?":"toy_eq$"
+}
 /*
 	embed function
 */
 var toy_add$ = function(arg1, arg2)
 {
-	if(typeof(arg1) == "number" && typeof(arg2) == "number")
-		return arg1 + arg2;
-	if(typeof(arg1) == 'string' || typeof(arg2) == 'string')
-		return arg1 + arg2;
+	return arg1 + arg2;
 }
 var toy_sub$ = function(arg1, arg2)
 {
-	if(typeof(arg1) == "number" && typeof(arg2) == "number")
-		return arg1 + arg2;
+	return arg1 - arg2;
 }
 var toy_mul$ = function(arg1, arg2)
 {
-	if(typeof(arg1) == "number" && typeof(arg2) == "number")
-		return arg1 + arg2;
+	   return arg1 * arg2;
 }
 var toy_div$ = function(arg1, arg2)
 {
-	if(typeof(arg1) == "number" && typeof(arg2) == "number")
-		return arg1 + arg2;
+	   return arg1 / arg2;
 }
 var toy_rem$ = function(arg1, arg2)
 {
-	if(typeof(arg1) == "number" && typeof(arg2) == "number")
-		return arg1 + arg2;
+	   return arg1 % arg2;
 }
 var ref = function(arg, key)
 {
     return arg[key];
 }
-var add = function(a,b){return a+b;}
+var toy_lt$ = function(arg1, args)
+{
+    return arg1 < arg2;
+}
+var toy_eq$ = function(arg1, arg2)
+{
+    return arg1 === arg2;
+}
+
+/*
+    interpret directly
+*/
+var REF = 1; // ref var_name at_frame_index_of_env
+var CONSTANT = 2; // constant value
+var ASSIGN = 3; // assign var_name at_frame_index_of_env
+var RETURN = 4; // RETURN 0 0
+var CLOSURE = 5; // CLOSURE return_place
+var FRAME = 6; // create new frame
+var ARGUMENT = 7; // push argument
+var CALL = 8;     // call 
+var TEST = 9; // test jmp
+var JMP = 10; // jmp jump steps
+var lookup_env = function(symbol_table, var_name, instructions)
+{
+    for(var i = symbol_table.length-1; i>=0; i--)
+    {
+        if(var_name in symbol_table[i])
+        {
+            instructions.push([REF, var_name, i]);
+            return;
+        }
+    }
+    console.log("Cannot find var : "+var_name);
+    return;
+}
+var another_compiler_lambda = function(args, body, symbol_table, instructions)
+{
+    // begin closure
+    var index = instructions.length;
+    instructions.push([CLOSURE, 0, 0]);
+
+    symbol_table.push({});
+    // add args
+    while(!args.NULL)
+    {
+        symbol_table[symbol_table.length - 1][car(args)] = true;
+        args = cdr(args);
+    }
+    another_compiler_seq(body, symbol_table, instructions);
+    // add return
+    instructions.push([RETURN, 0, 0]);
+    instructions[index][1] = instructions.length - 1; // set return index
+    return;
+}
+var another_compiler_applic = function(head, params, symbol_table, instructions)
+{
+    instructions.push([FRAME, 0, 0]);
+    // compile parameters
+    while(!params.NULL)
+    {
+        var v = car(params);
+        another_compiler(v, symbol_table, instructions);
+        instructions.push([ARGUMENT, 0, 0]);
+        params = cdr(params);
+    }
+    // compile head
+    another_compiler(head, symbol_table, instructions);
+    instructions.push([CALL, 0, 0]);// call procedure
+    return;
+}
+/*
+    (if 1 2 3)
+    CONSTANT 1
+    TEST 3 0
+    CONSTANT 2
+    JMP 2
+    CONSTANT 3
+
+*/
+var another_compiler_if = function(test, consequent, alternative, symbol_table, instructions)
+{
+    var index1 = 0;
+    instructions.push([TEST, 0, 0]);
+    // compile consequent
+    another_compiler(consequent, symbol_table, instructions);
+
+    var index2 = instructions.length; 
+    instructions.push([JMP, 0, 0]);
+    // compile alternative
+    another_compiler(alternative, symbol_table, instructions);
+    instructions[index2][1] = instructions.length - index2;
+    instructions[index1][1] = index2+1;
+    return;
+}
+var another_compiler_seq = function(exps, symbol_table, instructions)
+{
+    if(exps.NULL)
+        return instructions;
+    else
+    {
+        another_compiler(car(exps), symbol_table, instructions);
+        return another_compiler_seq(cdr(exps), symbol_table, instructions);
+    }
+}
+var another_compiler = function(exp, symbol_table, instructions)
+{
+    if (isNumber(exp))     // number
+    {
+        instructions.push([CONSTANT, parseFloat(exp), 0]);
+        return;
+    }
+    else if(typeof(exp) === 'string') // string
+        return lookup_env(symbol_table, exp, instructions);
+    else // lisp
+    {
+        var tag = car(exp);
+        if(tag === "quote")
+        {
+            var v = cadr(exp);
+        }
+        else if (tag === "define")
+        {
+            if(cadr(exp).TYPE === LIST)
+                return another_compiler(make_lambda(exp), symbol_table, instructions);
+            var var_name = cadr(exp);
+            var var_value = caddr(exp);
+            another_compiler(var_value, symbol_table, instructions);
+            instructions.push([ASSIGN, var_name, symbol_table.length - 1]) // push instructions
+            symbol_table[symbol_table.length - 1][var_name] = true; // add var name to symbol table
+            return;
+        }
+        else if (tag === "set!")
+        {
+            var var_name = cadr(exp);
+            var var_value = caddr(exp);
+            another_compiler(var_value, symbol_table, instructions); // compile var value
+            /* check var_name in symbol table */
+            for(var i = symbol_table.length - 1; i>=0; i--)
+            {
+                if(var_name in symbol_table[i])
+                {
+                    instructions.push([ASSIGN, var_name, i])
+                    return;
+                }
+            }
+            console.log("Unbound variable name: " + var_name);
+            return;
+        }
+        else if (tag === "if")
+        {
+            return another_compiler_if(if_test(exp), if_consequent(exp), if_alternative(exp), symbol_table, instructions);
+        }
+        else if (tag === "lambda")
+        {
+            return another_compiler_lambda(lambda_arguments(exp), lambda_body(exp), symbol_table.slice(0), instructions);
+        }
+        else
+        {
+            return another_compiler_applic(application_head(exp), application_args(exp), symbol_table.slice(0), instructions);
+        }
+    }
+}
+var another_interpreter = function(insts, env, acc, stack, pc)
+{
+    if(pc === insts.length)
+        return acc;
+    var inst = insts[pc];
+    var op = inst[0];
+    var arg0 = inst[1];
+    var arg1 = inst[2];
+    if(op === REF)
+        return another_interpreter(insts, env, env[arg1][arg0], stack, pc+1);
+    else if (op === CONSTANT)
+        return another_interpreter(insts, env, arg0, stack, pc+1);
+    else if (op === ASSIGN)
+    {
+        env[arg1][arg0] = acc;
+        return another_interpreter(insts, env, acc, stack, pc+1)
+    }
+    else if (op === RETURN)
+    {
+        return acc;
+    }
+    else if (op === CLOSURE)
+    {
+        var v = ["closure", pc+1, env.slice(0)]; // make closure... temp
+        return another_interpreter(insts, env, v, stack, arg0); // jmp
+    }
+    else if (op === FRAME) // add frame
+    {
+        stack.push([]);
+        return another_interpreter(insts, env, acc, stack, pc+1);
+    }
+    else if (op === ARGUMENT) // push argument
+    {
+        stack[stack.length - 1].push(acc);
+        return another_interpreter(insts, env, acc, stack, pc+1);
+    }
+    else if (op === CALL)
+    {
+        var v = another_interpreter(insts, acc[2], null, stack, acc[1]);
+        // pop frame
+        stack.pop();
+        return another_interpreter(insts, env, v, stack, pc+1);
+    }
+    else if (op === TEST)
+    {
+        if(acc == null)
+            return another_interpreter(insts, env, acc, stack, pc+arg0);
+        else return another_interpreter(insts, env, acc, stack, pc+1);
+    }
+    else if (op === JMP)
+    {
+        return another_interpreter(insts, env, acc, stack, pc+arg0);
+    }
+    else
+    {
+        console.log("Error...invalid instructions opcode " + op);
+    }
+}
+var displayInsts = function(insts)
+{
+    for(var i = 0; i < insts.length; i++)
+    {
+        var op = insts[i][0];
+        if(op === REF)
+            console.log("REF " + insts[i][1] + " " + insts[i][2]);
+        if(op === CONSTANT)
+            console.log("CONSTANT " + insts[i][1] + " " + insts[i][2]);
+        if(op === ASSIGN)
+            console.log("ASSIGN " + insts[i][1] + " " + insts[i][2]);
+        if(op === RETURN)
+            console.log("RETURN " + insts[i][1] + " " + insts[i][2]);
+        if(op === CLOSURE)
+            console.log("CLOSURE " + insts[i][1] + " " + insts[i][2]);
+        if(op === FRAME)
+            console.log("FRAME " + insts[i][1] + " " + insts[i][2]);
+        if(op === ARGUMENT)
+            console.log("ARGUMENT " + insts[i][1] + " " + insts[i][2]);
+        if(op === CALL)
+            console.log("CALL " + insts[i][1] + " " + insts[i][2]);
+        if(op === TEST)
+            console.log("TEST " + insts[i][1] + " " + insts[i][2]);
+        if(op === JMP)
+            console.log("JMP " + insts[i][1] + " " + insts[i][2]);
+    }
+}
 
 /*
 var x = "(+ 3 4)"
@@ -823,8 +1070,16 @@ if (typeof(module)!="undefined"){
 }
 
 
+var instructions = [];
+var symbol_table = [{
+    "+":true, "-":true, "*":true, "/":true
+}]
 
-
+var x = "(define (add a b) (+ a b) ) (add 3 4)"
+var l = lexer(x);
+var p = parser(l);
+var o = another_compiler_seq(p, symbol_table, instructions);
+displayInsts(instructions);
 
 
 
